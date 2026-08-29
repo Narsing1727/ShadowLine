3c8f279d0aa97fb8eaaf3723482c7e8d7d9cf73e
Newton1727 <narsing_s@ce.iitr.ac.in>
2026-08-29 19:48:11 +0530

    Clean up temporary PPTX extraction files

--- extract_bg.py
+++
@@ -1,7 +0,0 @@
-import zipfile
-import re
-
-with zipfile.ZipFile('UpStream_ShadowLine.pptx', 'r') as z:
-    slide_xml = z.read('ppt/slides/slide1.xml').decode('utf-8')
-    bg = re.findall(r'<p:bg>.*?</p:bg>', slide_xml, re.DOTALL)
-    print("Background XML:", bg)
